c1c372ef20953dce2078dbcd2a3959d645939625
jjmonte20 <jjmonte20@gmail.com>
2019-06-19 10:18:34 -0500

    organized app.js to separate event listener and defined function

--- app.js
+++ app.js
@@ -1,11 +1,4 @@
-window.addEventListener('keydown', function(e) {
-    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
-    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
-    if(!audio) return; // stop the function from running unneccessarily
-    audio.currentTime = 0; // rewind to the start
-    audio.play();
-    key.classList.add('playing');
-});
+window.addEventListener('keydown', playSound);
 
 function removeTransition(e){
     if(e.propertyName !== 'transform') return;
@@ -15,3 +8,11 @@ function removeTransition(e){
 const keys = document.querySelectorAll('.key');
 keys.forEach(key => key.addEventListener('transitionend', removeTransition));
 
+function playSound(e) {
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+    if(!audio) return; // stop the function from running unneccessarily
+    audio.currentTime = 0; // rewind to the start
+    audio.play();
+    key.classList.add('playing');
+}
