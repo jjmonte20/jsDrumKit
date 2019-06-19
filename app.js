6e31b812bd66c12b6228f0566437b6562a86e22f
jjmonte20 <jjmonte20@gmail.com>
2019-06-19 09:59:33 -0500

    able to rewind to zero the audio files, more accurate playing

--- app.js
+++ app.js
@@ -2,6 +2,6 @@ window.addEventListener('keydown', function(e) {
     const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
     console.log(audio);
     if(!audio) return; // stop the function from running unneccessarily
-
+    audio.currentTime = 0; // rewind to the start
     audio.play();
 });
