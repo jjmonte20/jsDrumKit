186f3b16d424905742b25e77db65bbe352e01092
jjmonte20 <jjmonte20@gmail.com>
2019-06-19 10:05:33 -0500

    able to toggle keys on the html page to go from not playing to playing

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 window.addEventListener('keydown', function(e) {
     const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
-    console.log(audio);
+    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+    console.log(key);
     if(!audio) return; // stop the function from running unneccessarily
     audio.currentTime = 0; // rewind to the start
     audio.play();
+    key.classList.add('playing');
 });
